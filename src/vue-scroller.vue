cbdf97a354885615791024bc76c2606c1346af25
Nguyen Quang Anh <quanganhcnt50@gmail.com>
2020-12-08 15:42:02 +0700

    feat✨ add one more type

--- src/vue-scroller.vue
+++ src/vue-scroller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="is-flex-col">
+  <div v-if="buttonType === 'fab'">
     <el-tooltip
       class="item"
       effect="dark"
@@ -42,6 +42,47 @@
       ></el-button>
     </el-tooltip>
   </div>
+  <div v-else>
+    <el-tooltip
+      class="item"
+      effect="dark"
+      content="Về đầu văn bản"
+      placement="top"
+    >
+      <el-button
+        @click="$emit('up')"
+        size="mini"
+        class="movefast"
+        icon="el-icon-d-arrow-left"
+      >Cuộn lên đầu</el-button>
+    </el-tooltip>
+    <el-tooltip
+      v-if="!disableAutoScroll"
+      class="item"
+      effect="dark"
+      content="Tự động cuộn"
+      placement="top"
+    >
+      <el-button
+        @click="$emit('input', !value)"
+        size="mini"
+        :icon="value ? 'el-icon-video-pause' : 'el-icon-arrow-down'"
+      >{{ value ? 'Tắt tự động cuộn' : 'Bật tự động cuộn' }}</el-button>
+    </el-tooltip>
+    <el-tooltip
+      class="item"
+      effect="dark"
+      content="Về cuối văn bản"
+      placement="top"
+    >
+      <el-button
+        @click="$emit('down')"
+        size="mini"
+        class="movefast"
+        icon="el-icon-d-arrow-right"
+      >Cuộn xuống cuối</el-button>
+    </el-tooltip>
+  </div>
 </template>
 
 <script>
@@ -54,6 +95,10 @@ export default {
     disableAutoScroll: {
       type: Boolean,
       default: false,
+    },
+    buttonType: {
+      type: String,
+      default: 'fab', // default, fab
     },
   },
 };
